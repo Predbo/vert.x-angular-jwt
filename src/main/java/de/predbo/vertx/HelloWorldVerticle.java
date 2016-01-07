487efcef2e631dc68db9be0055c4f1cf27472fe8
Stefan Hanke <shanke@predbo.de>
2016-01-07 18:46:36 +0100

    add second server without using Java 8 lamda expression, just to see differences

--- src/main/java/de/predbo/vertx/HelloWorldVerticle.java
+++ src/main/java/de/predbo/vertx/HelloWorldVerticle.java
@@ -1,13 +1,31 @@
 package de.predbo.vertx;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
 
 public class HelloWorldVerticle extends AbstractVerticle {
 
-  @Override
-  public void start() {
-    // Create an HTTP server which simply returns "Hello World!" to each request.
-    vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
-  }
+	@Override
+	public void start() {
+
+		// Create an HTTP server which simply returns "Hello World!" to each request.
+		vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
+
+		createHttpServerWithoutJava8Lamdas();
+	}
+
+	private void createHttpServerWithoutJava8Lamdas() {
+		HttpServer server = vertx.createHttpServer();
+
+		server.requestHandler(new Handler<HttpServerRequest>() {
+			public void handle(HttpServerRequest request) {
+				System.out.println("A request has arrived on the server!");
+				request.response().end("Hello World!");
+			}
+		});
+
+		server.listen(8081, "localhost");
+	}
 }
